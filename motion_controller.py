ea51a6c0dba19732197e0565037e38c9e93cf70e
J.Coleman <jack.coleman@monashmotorsport.com>
2020-05-10 18:55:38 +1000

    Added P controller based solution

--- motion_controller.py
+++ motion_controller.py
@@ -10,7 +10,9 @@ class MotionController():
         """
             The constructor function called when you make an object of type MotionController()
         """
-        self.time = 0
+        self._throttle_Kp = 1.0
+        self._steering_Kp = 1.0
+        self._lookahead_index = 2
 
     def get_controls(self, car_state, path_reference):
         """
@@ -33,11 +35,18 @@ class MotionController():
                               path_reference[0].y is a number that is the y coordinate of the point
 
         """
+        vel_error = path_reference[self._lookahead_index].vel - car_state.velocity
+        throttle = self._throttle_Kp * vel_error
 
-        # For dummy purposes just to show that the car moves (make the car move randomly)
-        self.time += 0.02
-        steering = -math.sin(self.time*0.1)
-        throttle = math.sin(self.time)
+        target_vec_x = path_reference[self._lookahead_index].x - car_state.x_pos
+        target_vec_y = path_reference[self._lookahead_index].y - car_state.y_pos
 
+        target_yaw = math.atan2(target_vec_y, target_vec_x)
+
+        angle_error = wrap_angle(target_yaw - car_state.yaw)
+
+        steering = self._steering_Kp * angle_error
+        
+        # print(f"target yaw: {math.degrees(target_yaw):4.0f}, current yaw: {math.degrees(car_state.yaw):4.0f}, angle error: {math.degrees(angle_error):4.0f}")
         # Controls are of the form (steering (between [-1, 1]), throttle (between [-1, 1]))
         return Controls(steering, throttle)
